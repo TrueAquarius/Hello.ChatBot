2a6b196a17d3ef7267873bbbea6d9be51745cb83
TrueAquarius <98264396+TrueAquarius@users.noreply.github.com>
2025-06-01 10:39:47 +0200

    Version number

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -4,7 +4,7 @@ public static class BuildInfo
 {
     public const string BuildDate = "01.06.2025 10:28 +02:00";
     public const int BuildNumber = 435;
-    public const string Version = "V1.0.4";
+    public const string Version = "V1.0.5";
 
     public static string All { get { return Version + " (Build " + BuildNumber + ")"; } }
 }
